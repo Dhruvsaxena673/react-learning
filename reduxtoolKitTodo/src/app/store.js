30d2290ebf0f893e862b5714b80ac693f64bf7c3
Dhruv Saxena <dhruvsaxena673@gmail.com>
2026-09-15 03:30:53 +0530

    redux toolKit project

---
+++ reduxtoolKitTodo/src/app/store.js
@@ -0,0 +1,6 @@
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
+// single source of truth
+export const store = configureStore({
+    reducer: todoReducer
+})
